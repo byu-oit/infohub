fa9ac93b2521b17eb7bf93ecc7f6270525d42c57
JeremyRees <js.rees@yahoo.com>
2019-02-11 17:36:05 -0700

    Fix time zone

--- app/Config/core.php
+++ app/Config/core.php
@@ -440,7 +440,7 @@ if (!function_exists('denv')) {
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	date_default_timezone_set('UTC');
+	date_default_timezone_set('America/Denver');
 
 /**
  * `Config.timezone` is available in which you can set users' timezone string.
