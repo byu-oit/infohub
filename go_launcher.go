e4b9a42409ad808b16dc6307807c50c70c294141
Nate Peterson <nate_peterson@byu.edu>
2019-09-16 14:49:02 -0600

    Moving Prod to AWS (#5)
    * initial aws config
    
    * fixing yml formatting
    
    * fixing dependencies
    
    * basic ping page
    
    * updated pipeline
    
    * fixing buildspec
    
    * add dynamic config
    
    * fixed dockerfile
    
    * trying binary launcher
    
    * Dockerfile and launcher improvements
    
    * increasing cpu
    
    * debug ping.php page
    
    * remove debug
    
    * change to core-local instead of env
    
    * Remove the manual exit
    
    * fix collibra config
    
    * attempting stdout logging
    
    * test logging
    
    * Logging working
    
    * testing a log message
    
    * remove test log
    
    * first shot at ecs
    
    * adding debug prints for storage
    
    * adding explicit volume to dockerfile
    
    * ignore error
    
    * make mount dir
    
    * rollback mountpoint
    
    * efs uploads dir
    
    * updating the cluster
    
    * moving dev pipeline
    
    * Display entire space on one page
    
    * Fix formatting for space on single page
    
    * missing newline
    
    * Include space relation with dataset
    
    * Allow importing virtual datasets
    
    * adding dremio credential loading
    
    * removing debug statements
    
    * Tidy up request view
    
    * Don't cover last line of form with submit button
    
    * Don't cover cancel button on submit
    
    * Fix virtual dataset auto-matching
    
    * Allow destructive update of APIs
    
    * Allow updates of requested API if nothing changed
    
    * Accommodate users not being logged in when importing Swagger
    
    * Destructive updates of data warehouse
    
    Including notifications if the hourly sync job finds a table has changed
    
    * Destructive updates of virtual datasets
    
    * Collibra 5.6 upgrade change
    
    * Make GRO group OR logic explicit
    
    * Return PDF to be attached in workflow
    
    * Change virtual dataset structure
    
    * Query CESPRD database
    
    * Revert "Change virtual dataset structure"
    
    This reverts commit 51df1e2a720d1814800106ff8568150e90c06e89.
    
    * Don't try to access definition in searchStandardLabel
    
    * Test CAS 5 upgrade
    
    * Revert "Test CAS 5 upgrade"
    
    This reverts commit e6410aaa1fad4e0cf68a8b6c751af110cb2d1872.
    
    * Partial changes to virtual datasets
    
    We're changing how we're organizing virtual datasets in Collibra. This commit excludes some features—such as importing, matching terms, and editing virtual table to DSR relations—because there is a dev shop wanting to get a DSR through as soon as possible.
    
    * Finished changes to virtual dataset structure
    
    * Update rejected DSR display
    
    * Add Alternative Field Name attribute
    
    * Use search words to track alternative field names
    
    * UI for multiple API hosts
    
    * Only check search words when autocomplete empty
    
    * Disregard version in Swagger document
    
    * View requested SAML fields
    
    * Only search previously matched field names
    
    * Fix URL encoding of post data if values contain json
    
    * updating pipeline in prep for prod deploy
    
    * updated pipeline branch
    
    * fixing pipeline
    
    * added dns
    
    * updating containers
    
    * trying to split the task on two container
    
    * tweaking sizing
    
    * scaling up
    
    * adding to gitignore and vendor changes
    
    * Revert "adding to gitignore and vendor changes"
    
    This reverts commit ffa723990c2a58e8e06d67b197e235280760e613.
    
    * updating pipeline to point to master

---
+++ go_launcher.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+// Config struct stores the config needed for the config-local file
+type Config struct {
+	Debug            int
+	URL              string
+	CollibraURL      string
+	CollibraUser     string
+	CollibraPassword string
+	ByuAPIKey        string
+	ByuAPISecret     string
+	DremioURL        string
+	DremioUser       string
+	DremioPassword   string
+	GithubToken      string
+	DBHost           string
+	DBUser           string
+	DBPassword       string
+	DBName           string
+	Salt             string
+	CipherSeed       string
+}
+
+func main() {
+	log.Println("Starting Config Launcher")
+	path := os.Getenv("HANDEL_PARAMETER_STORE_PATH")
+	if path == "" {
+		log.Println("No Path Enviroment Variable found")
+		log.Println("Linking Uploads Directory")
+		linkDir()
+		run()
+	}
+	log.Println("Path:", path)
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config:            aws.Config{Region: aws.String("us-west-2")},
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	ssmsvc := ssm.New(sess)
+
+	log.Println("Fetching Parameters at path:", path)
+	params := getPathParameters(ssmsvc, &path)
+	debug, _ := strconv.Atoi(params["debug"])
+	config := Config{
+		Debug:            debug,
+		URL:              params["url"],
+		CollibraURL:      params["collibra_url"],
+		CollibraUser:     params["collibra_user"],
+		CollibraPassword: params["collibra_password"],
+		ByuAPIKey:        params["byuapi_key"],
+		ByuAPISecret:     params["byuapi_secret"],
+		DremioURL:        params["dremio_url"],
+		DremioUser:       params["dremio_user"],
+		DremioPassword:   params["dremio_password"],
+		GithubToken:      params["github_token"],
+		DBHost:           os.Getenv("DB_ADDRESS"),
+		DBUser:           params["db_username"],
+		DBPassword:       params["db_password"],
+		DBName:           params["db_name"],
+		Salt:             params["salt"],
+		CipherSeed:       params["cipher"],
+	}
+
+	log.Println("Creating core-local.php")
+	makeTemplate(config)
+	log.Println("Linking Uploads Directory")
+	linkDir()
+	log.Println("Starting Apache2")
+	run()
+}
+
+func run() {
+	//Clean up potential conflict
+	os.Remove("/run/apache2/httpd.pid")
+
+	cmd := exec.Command("/usr/sbin/httpd", "-D", "FOREGROUND")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Finished")
+}
+
+func linkDir() {
+	olddir := os.Getenv("STORAGE_MOUNT_DIR")
+	newdir := "/cake/app/webroot/uploads"
+	err := os.Symlink(olddir, newdir)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func makeTemplate(config Config) {
+	f, err := os.Create("app/Config/core-local.php")
+	if err != nil {
+		log.Println("Failed to create file: ", err)
+	}
+	defer f.Close()
+	t := template.Must(template.ParseFiles("go_core-local.gophp"))
+	t.Execute(f, config)
+}
+
+func getPathParameters(ssmsvc *ssm.SSM, path *string) map[string]string {
+	m := make(map[string]string, 0)
+	err := ssmsvc.GetParametersByPathPages(&ssm.GetParametersByPathInput{
+		Path:           path,
+		WithDecryption: aws.Bool(true),
+	}, func(params *ssm.GetParametersByPathOutput, lastPage bool) bool {
+		for _, param := range params.Parameters {
+			name := strings.TrimPrefix(*param.Name, *path)
+			m[name] = *param.Value
+		}
+		return !lastPage
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
+}
+
+func getParameter(ssmsvc *ssm.SSM, paramName *string) string {
+	param, err := ssmsvc.GetParameter(&ssm.GetParameterInput{
+		Name:           paramName,
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return *param.Parameter.Value
+}
